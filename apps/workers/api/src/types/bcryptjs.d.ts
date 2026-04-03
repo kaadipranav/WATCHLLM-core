4f90016e8c3082e5e852899705ed22329dc55502
kaadipranav <kiwi092020@gmail.com>
2026-04-03 06:36:31 +0530

    fixed

--- apps/workers/api/src/types/bcryptjs.d.ts
+++ apps/workers/api/src/types/bcryptjs.d.ts
@@ -1,7 +1,9 @@
 declare module 'bcryptjs' {
+  export function hash(value: string, rounds: number): Promise<string>;
   export function compare(value: string, hash: string): Promise<boolean>;
 
   const bcrypt: {
+    hash(value: string, rounds: number): Promise<string>;
     compare(value: string, hash: string): Promise<boolean>;
   };
 
